6ebbcbb6607bf8deb1cb38ed78002da50ac4946c
nandemotoken <46263082+nandemotoken@users.noreply.github.com>
2021-08-06 17:00:22 +0900

    Update JPYC_example1_moneybox.sol

--- JPYC_example1_moneybox.sol
+++ JPYC_example1_moneybox.sol
@@ -6,6 +6,7 @@ pragma solidity ^0.8.0;
 
 //ERC20規格を読み込むための準備
 interface IERC20 {
+    //標準的なインタフェース
     function totalSupply() external view returns (uint256);
     function balanceOf(address account) external view returns (uint256);
     function transfer(address recipient, uint256 amount) external returns (bool);
@@ -15,7 +16,7 @@ interface IERC20 {
     event Transfer(address indexed from, address indexed to, uint256 value);
     event Approval(address indexed owner, address indexed spender, uint256 value);
     
-    //呼び出したい関数を指定する
+    //追加で呼び出したい関数を指定しています
     function name() external view returns (string memory);
     function symbol() external view returns (string memory);
 }
